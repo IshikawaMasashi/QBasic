bf47229f7d1e3a3cb5c1d8edd1932e1584e502b4
雅史 石川 <a622740@gmail.com>
2019-01-19 09:35:28 +0900

    ServiceWorkerを追加

---
+++ serviceWorker.js
@@ -0,0 +1,28 @@
+﻿var CACHE_NAME = "MKit-cache-v1";
+var urlsToCache = [
+  "https://ishikawamasashi.github.io/QBasic/",
+  "https://ishikawamasashi.github.io/QBasic/index.html"
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(
+      function (cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(
+      function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
