f8623bcdd6a242afebe5c80387c293c7407d8729
雅史 石川 <a622740@gmail.com>
2019-01-29 07:42:10 +0900

    修正

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-﻿var CACHE_NAME = "MKit-cache-v1";
+﻿var CACHE_NAME = "cache-v2";
 var urlsToCache = [
   "https://ishikawamasashi.github.io/QBasic/",
   "https://ishikawamasashi.github.io/QBasic/index.html",
@@ -79,25 +79,66 @@ var urlsToCache = [
   "https://ishikawamasashi.github.io/QBasic/style/global.css",
 ];
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(
-      function (cache) {
+      .then((cache) => {
+        console.log('Opened cache');
+
+        // 指定されたリソースをキャッシュに追加する
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('activate', (event) => {
+  var cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          // ホワイトリストにないキャッシュ(古いキャッシュ)は削除する
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(
-      function (response) {
+      .then((response) => {
         if (response) {
           return response;
         }
-        return fetch(event.request);
+
+        // 重要：リクエストを clone する。リクエストは Stream なので
+        // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
+        // 必要なので、リクエストは clone しないといけない
+        let fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest)
+          .then((response) => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // 重要：レスポンスを clone する。レスポンスは Stream で
+            // ブラウザ用とキャッシュ用の2回必要。なので clone して
+            // 2つの Stream があるようにする
+            let responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          });
       })
   );
 });
