e78099bf940bf0d141e7a7210729ab22a1b3bf77
雅史 石川 <a622740@gmail.com>
2020-01-31 11:50:57 +0900

    修正

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-﻿var CACHE_NAME = "QBasic-cache-v23";
+﻿var CACHE_NAME = "QBasic-cache-v24";
 var urlsToCache = [
   "https://ishikawamasashi.github.io/QBasic/",
   "https://ishikawamasashi.github.io/QBasic/index.html",
